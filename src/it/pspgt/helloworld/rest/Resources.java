0fcf61be22cd831136ef816dbf991e9eb8501862
pspgt <gabry.t92@gmail.com>
2016-08-26 09:31:27 +0200

    Aggiunto la possibilità di inviare parametri tramite la GET

--- src/it/pspgt/helloworld/rest/Resources.java
+++ src/it/pspgt/helloworld/rest/Resources.java
@@ -2,15 +2,16 @@ package it.pspgt.helloworld.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 @Path("helloworld/1.0")
 public class Resources {
 
 	@GET
-	@Path("hello")
+	@Path("hello/{id}")
 	@Produces("text/plain;charset=UTF-8")
-	public String hello(){
-		return "HelloWorld Rest tramite OSGi e Jetty";
+	public String hello(@PathParam("id")String id){
+		return id+" HelloWorld Rest tramite OSGi e Jetty";
 	}
 }
